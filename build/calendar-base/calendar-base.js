549648183928adb921b4c456c823c74eddf74452
Jonathan Mak <jonathan.mak@liferay.com>
2017-08-02 11:31:22 -0700

    AUI-1205 - Build

--- build/calendar-base/calendar-base.js
+++ build/calendar-base/calendar-base.js
@@ -411,6 +411,7 @@ Y.CalendarBase = Y.extend( CalendarBase, Y.Widget, {
      * @private
      */
     _addDateToSelection : function (oDate, index) {
+        oDate = this._normalizeTime(oDate);
 
         if (this._canBeSelected(oDate)) {
 
